508bfef47337227022bd0445551fbe1aad8179e0
Justin Nuß <nuss.justin@gmail.com>
2025-05-01 19:47:33 +0200

    esiexpr: add package doc comment

---
+++ esiexpr/doc.go
@@ -0,0 +1,4 @@
+// Package esiexpr implements parsing of ESI expressions and variables.
+//
+// It also provides an implementation of the [esi.Env] interface that handles ESI expressions.
+package esiexpr
